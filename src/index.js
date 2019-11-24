eabffc80c7a42746a0f21b0ee95ec4d1242d7543
clarkj99 <clark@stontech.net>
2019-11-23 19:14:22 -0500

    working on dog-info

---
+++ src/index.js
@@ -0,0 +1,40 @@
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('hello')
+    const dogBarDiv = document.querySelector('#dog-bar')
+
+    function pupSpan(pup) {
+        const oldDiv = document.querySelector('#dog-info');
+        const span = document.createElement('span');
+        span.innerText = pup.name;
+        span.addEventListener('click', (event) => {
+            // console.log(pupInfo(pup))
+            oldDiv.parentElement.replaceChild(pupInfo(pup), oldDiv)
+        })
+        return span;
+    }
+
+    function pupInfo(pup) {
+
+        const img = document.createElement('img');
+        img.src = pup.image;
+        const h2 = document.createElement('h2');
+        h2.innerText = pup.name;
+        const button = document.createElement('button');
+        button.innerText = pup.isGoodDog ? 'Good Dog!' : 'Bad Dog!';
+        const newDiv = document.createElement('div');
+        newDiv.id = 'dog-info';
+        newDiv.appendChild(img);
+        newDiv.appendChild(h2);
+        newDiv.appendChild(button);
+        return newDiv;
+    }
+
+    fetch('http://localhost:3000/pups')
+        .then(res => res.json())
+        .then((data) => {
+            data.forEach(pup => {
+                dogBarDiv.appendChild(pupSpan(pup))
+            })
+
+        })
+})
