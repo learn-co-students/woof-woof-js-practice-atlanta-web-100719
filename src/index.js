a4eb2d94e832bf85e9bad7bcf8ecbcf7f69a312f
clarkj99 <clark@stontech.net>
2019-11-23 20:01:27 -0500

    Toggle

--- src/index.js
+++ src/index.js
@@ -6,13 +6,25 @@ window.addEventListener('DOMContentLoaded', () => {
         const span = document.createElement('span');
         span.innerText = pup.name;
         span.addEventListener('click', (event) => {
-            const oldDiv = document.querySelector('#dog-info');
-            // console.log(pupInfo(pup))
-            oldDiv.parentElement.replaceChild(pupInfo(pup), oldDiv)
+            displayInfo(pup.id)
         })
         return span;
     }
 
+    function dogText(pup) {
+        return pup.isGoodDog ? 'Good Dog!' : 'Bad Dog!';
+    }
+
+    function displayInfo(pupId) {
+        fetch('http://localhost:3000/pups/' + pupId)
+            .then(res => res.json())
+            .then(pup => {
+                const oldDiv = document.querySelector('#dog-info');
+                // console.log(pupInfo(pup))
+                oldDiv.parentElement.replaceChild(pupInfo(pup), oldDiv)
+            })
+
+    }
     function pupInfo(pup) {
 
         const img = document.createElement('img');
@@ -20,7 +32,26 @@ window.addEventListener('DOMContentLoaded', () => {
         const h2 = document.createElement('h2');
         h2.innerText = pup.name;
         const button = document.createElement('button');
-        button.innerText = pup.isGoodDog ? 'Good Dog!' : 'Bad Dog!';
+        button.innerText = dogText(pup);
+        button.addEventListener('click', (event) => {
+            newText = event.target.innerText == 'Bad Dog!' ? 'Good Dog!' : 'Bad Dog!';
+
+            event.target.innerText = newText;
+
+            fetch('http://localhost:3000/pups/' + pup.id, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ isGoodDog: !pup.isGoodDog })
+            })
+                .then(res => res.json())
+                .then((data) => {
+                    console.log(data);
+                    event.target.innerText = dogText(data);
+                })
+
+        })
         const newDiv = document.createElement('div');
         newDiv.id = 'dog-info';
         newDiv.appendChild(img);
@@ -28,6 +59,8 @@ window.addEventListener('DOMContentLoaded', () => {
         newDiv.appendChild(button);
         return newDiv;
     }
+
+
 
     fetch('http://localhost:3000/pups')
         .then(res => res.json())
